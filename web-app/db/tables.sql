9173ae204c842a40b12dcfcd070c15ebfb6eb194
damonyu <damonyu97@hotmail.com>
2022-07-07 01:09:43 +0100

    update database user table

--- web-app/db/tables.sql
+++ web-app/db/tables.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS Users (
   UserID UUID PRIMARY KEY NOT NULL,
-  Name varchar(255) NOT NULL UNIQUE,
+  Name varchar(255) NOT NULL,
   Email varchar(255) NOT NULL UNIQUE,
-  Password varchar(255) NOT NULL
+  Password varchar(255) NOT NULL,
+  Status varchar(255) NOT NULL DEFAULT 'active' CHECK (Status in ('active', 'pending', 'deleted'))
 );
 
 CREATE TABLE IF NOT EXISTS Submissions (
@@ -11,10 +12,10 @@ CREATE TABLE IF NOT EXISTS Submissions (
   Name varchar(255) NOT NULL,
   Result text NOT NULL,
   Error varchar(255) NOT NULL,
-  
-  SubmitTime TIMESTAMP WITHOUT TIME ZONE NOT NULL 
+
+  SubmitTime TIMESTAMP WITHOUT TIME ZONE NOT NULL
   DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
-  
+
   FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
